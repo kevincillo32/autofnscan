01075705c85d3716e63524d5f72c27722a6cb2e1
Kevin Carballo Herrera <65192229+kevincillo32@users.noreply.github.com>
2023-01-16 09:37:05 +0000

    Update autonmaphtbv4.py

--- autonmaphtbv4.py
+++ autonmaphtbv4.py
@@ -1,24 +1,29 @@
 import os
 import re
 from termcolor import colored
+import subprocess
 
 # clear the console
+
 os.system('cls' if os.name == 'nt' else 'clear')
 
-# Clase para crear y mostrar informacion de las carpetas
 class Carpeta:
     def __init__(self, nombre):
         self.nombre = nombre
         os.mkdir(self.nombre)
     
-    def crear_carpeta(self, nombre_carpeta):
-        os.mkdir(self.nombre + '/' + nombre_carpeta)
-        print(colored(f"Carpeta {nombre_carpeta} creada en {self.nombre}", "green"))
+    def crear_carpeta(self, list_nombre_carpeta):
+        for nombre_carpeta in list_nombre_carpeta:
+            if os.path.exists(self.nombre + '/' + nombre_carpeta):
+                print(colored(f"La carpeta {nombre_carpeta} ya existe en {self.nombre}", "yellow"))
+            else:
+                os.mkdir(self.nombre + '/' + nombre_carpeta)
+                print(colored(f"Se ha creado la subcarpeta {nombre_carpeta} en {self.nombre}", "green"))
+
     
     def mostrar_info(self):
         print(colored(f"Información de la carpeta {self.nombre}:", "green"))
-        
-# Funcion para validar la direccion IP ingresada
+
 def validar_ip(ip_address):
     if re.match(r"^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$", ip_address):
         return True
@@ -26,7 +31,6 @@ def validar_ip(ip_address):
         print(colored("Dirección IP no válida. Inténtelo de nuevo.", "red"))
         return False
 
-# Funcion para determinar el sistema operativo de la maquina
 def determinar_sistema_operativo(ip_address):
     ping = os.popen("ping -c 3 " + ip_address).read()
     ttl = re.search(r"ttl=(\d+)", ping)
@@ -45,29 +49,37 @@ def determinar_sistema_operativo(ip_address):
     else:
         print(colored("No se pudo determinar el sistema operativo de la máquina", "red"))
 
-# Funcion para escanear todos los puertos TCP
 def escanear_puertos(ip_address, folder_name):
     nmap_cmd = "nmap -p- --open -sS --min-rate 5000 -vvv -n -Pn " + ip_address + " -oG " + folder_name + "/nmap/allPortsTCP"
     os.system(nmap_cmd)
     print(colored(f"El resultado del escaneo se ha guardado en {folder_name}/nmap/allPortsTCP", "green"))
+    extractPorts(f"{folder_name}/nmap/allPortsTCP")
 
-# Funcion para escanear solo los puertos abiertos
-def escanear_puertos_abiertos(ip_address, folder_name):
-    # Get user input for ports
-    ports = input("Ingresa los puertos que deseas escanear (separados por comas): ")
+def extractPorts(file_path):
+    with open(file_path, 'r') as f:
+        ports = [re.findall(r'open/(\d+)',line) for line in f if "open" in line]
+    ports = [i[0] for i in ports if i]
+    ports_str = ",".join(ports)
+    process = subprocess.Popen(['xclip', '-selection', 'clipboard'], stdin=subprocess.PIPE)
+    process.communicate(input=ports_str.encode('utf-8'))
+    print(colored(f"Los puertos abiertos se han copiado al portapeles: {ports_str}", "green"))
 
-    # Replace spaces with commas
-    ports = ports.replace(" ", ",")
 
-    # Scan the entered ports
-    nmap_cmd = f"nmap -sCV -p {ports} {ip_address} -oN {folder_name}/nmap/targeted"
+
+def escanear_puertos_personalizados(ip_address, folder_name):
+    puertos = input("Introduzca los puertos a escanear separados por comas: ")
+    nmap_cmd = f"nmap -sCV -p {puertos} {ip_address} -oN {folder_name}/nmap/targeted"
     os.system(nmap_cmd)
     print(colored(f"El resultado del escaneo se ha guardado en {folder_name}/nmap/targeted", "green"))
 
-    # Read the targeted file and get the open ports
-    with open(folder_name + '/nmap/targeted') as f:
-        puertos_abiertos = [line.split()[0].split("/")[0] for line in f if "open" in line]
-        puertos = ",".join(puertos_abiertos)
+# Main
+nombre_carpeta = input("Introduzca el nombre de la carpeta a crear: ")
+carpeta = Carpeta(nombre_carpeta)
+carpeta.crear_carpeta(['nmap','content', 'exploits', 'scripts'])
+ip_address = input("Introduce la direccion IP a escanear: ")
+if validar_ip(ip_address):
+    determinar_sistema_operativo(ip_address)
+    escanear_puertos(ip_address, nombre_carpeta)
+    escanear_puertos_personalizados(ip_address, nombre_carpeta)
 
-    # Copy the open ports to clipboard
-    os.system(f'echo {puertos} | xclip -sel clip')
+
