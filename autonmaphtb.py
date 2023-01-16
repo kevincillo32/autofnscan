ba1e006d604a7ffe2630b8b5e228dfb3d59e9ad1
Kevin Carballo Herrera <65192229+kevincillo32@users.noreply.github.com>
2023-01-15 22:44:15 -0600

    Update autonmaphtb.py

--- autonmaphtb.py
+++ autonmaphtb.py
@@ -1,8 +1,33 @@
 import os
 
-folder_name = input("Nombre del laboratorio: ")
-os.makedirs(folder_name+"/nmap/content/script/exploits", exist_ok=True)
-ip_address = input("Introduce la dirección IP a escanear: ")
-os.system("nmap -p- --open -sS --min-rate 5000 -vvv -n -Pn "+ip_address+" -oG "+folder_name+"/nmap")
-ports = os.popen("grep open "+folder_name+"/nmap/allPortsTCP | awk '{print $1}'").read().strip().replace("\n",",")
-os.system("nmap -sCV -p"+ports+" "+ip_address+" --oN "+folder_name+"/nmap/targeted")
+def main():
+    folder_name = input("Nombre del laboratorio: ")
+    create_folders(folder_name)
+    ip_address = input("Introduce la dirección IP a escanear: ")
+    nmap_scan(folder_name,ip_address)
+    ports = get_ports(folder_name)
+    nmap_scan_targeted(folder_name,ip_address,ports)
+
+def create_folders(folder_name):
+    """
+    Crea las carpetas necesarias para guardar los resultados del escaneo
+    """
+    os.makedirs(folder_name+"/nmap/content/script/exploits", exist_ok=True)
+
+def nmap_scan(folder_name,ip_address):
+    """
+    Realiza el escaneo con nmap y guarda los puertos escaneados en un archivo
+    """
+    os.system("nmap -p- --open -sS --min-rate 5000 -vvv -n -Pn "+ip_address+" -oG "+folder_name+"/nmap")
+
+def get_ports(folder_name):
+    """
+    Obtiene los puertos escaneados con nmap
+    """
+    return os.popen("grep open "+folder_name+"/nmap/allPortsTCP | awk '{print $1}'").read().strip().replace("\n",",")
+
+def nmap_scan_targeted(folder_name,ip_address,ports):
+    """
+    Realiza el escaneo detallado de los puertos escaneados y guarda el resultado en un archivo
+    """
+    os.system("nmap -sCV -p"+ports+" "+ip_address+" --oN "+folder_name+"/nmap/targeted")
