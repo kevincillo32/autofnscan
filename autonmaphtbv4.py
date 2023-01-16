06ad29e7126d2349e57fcde9261a4cad60872643
Kevin Carballo Herrera <65192229+kevincillo32@users.noreply.github.com>
2023-01-16 13:55:22 -0600

    Update autonmaphtbv4.py

--- autonmaphtbv4.py
+++ autonmaphtbv4.py
@@ -61,17 +61,15 @@ folder_name="carpeta"
 nmap_folder="subcarpeta"
 file_path="$folder_name/$nmap_folder/allPortsTCP"
 
-def extractPorts(){
-	cd "$folder_name/$nmap_folder"
-	ports="$(cat "$file_path" | grep -oP '\d{1,5}/open' | awk '{print $1}' FS='/' | xargs | tr ' ' ',')"
-	ip_address="$(cat "$file_path" | grep -oP '\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}' | sort -u | head -n 1)"
-	echo -e "\n[*] Extracting information...\n" > extractPorts.tmp
-	echo -e "\t[*] IP Address: $ip_address"  >> extractPorts.tmp
-	echo -e "\t[*] Open ports: $ports\n"  >> extractPorts.tmp
-	echo $ports | tr -d '\n' | xclip -sel clip
-	echo -e "[*] Ports copied to clipboard\n"  >> extractPorts.tmp
-	cat extractPorts.tmp; rm extractPorts.tmp
-}
+import subprocess
+
+folder_name = "carpeta"
+nmap_folder = "subcarpeta"
+file_path = f"{folder_name}/{nmap_folder}/allPortsTCP"
+
+def extractPorts():
+    subprocess.call(["bash", "-c", f"cd {folder_name}/{nmap_folder} && ports=$(cat {file_path} | grep -oP '\d{1,5}/open' | awk '{{print $1}}' FS='/' | xargs | tr ' ' ',') && ip_address=$(cat {file_path} | grep -oP '\d{{1,3}}.\d{{1,3}}.\d{{1,3}}.\d{{1,3}}' | sort -u | head -n 1) && echo -e '\n[*] Extracting information...\n' > extractPorts.tmp && echo -e '\t[*] IP Address: $ip_address'  >> extractPorts.tmp && echo -e '\t[*] Open ports: $ports\n'  >> extractPorts.tmp && echo $ports | tr -d '\n' | xclip -sel clip && echo -e '[*] Ports copied to clipboard\n'  >> extractPorts.tmp && cat extractPorts.tmp && rm extractPorts.tmp"])
+
 
 
 def escanear_puertos_personalizados(ip_address, folder_name):
